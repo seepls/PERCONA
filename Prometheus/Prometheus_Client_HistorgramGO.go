20a82f4473d53ca032fedc6dcb23ba07d9beb048
Smriti Tiwari <smrititiwari2016@gmail.com>
2020-02-07 22:44:51 +0530

    Update Prometheus_Client_HistorgramGO.go

--- Prometheus/Prometheus_Client_HistorgramGO.go
+++ Prometheus/Prometheus_Client_HistorgramGO.go
@@ -1,1 +1,20 @@
- 
+ import "github.com/prometheus/client_golang/prometheus"
+
+var jobDurationHistogram = prometheus.NewHistogramVec(
+    prometheus.HistorgramOpts{
+     Name:   "jobs_duration_seconds",
+     Help:   "Jobs_duration distribution",
+     Buckets:  []float64{1,2,5,10,20,60},
+    },
+ []string{"job_type"},
+)
+
+start := time.Now()
+job.Run()
+duration := time.Since(start)
+jobsDurationHistogram.WithLabelValues(job.Type()).Observe(duration.Seconds())
+
+prometheus.NewSummary()
+
+
+  
