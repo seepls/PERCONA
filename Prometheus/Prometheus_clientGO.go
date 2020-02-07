3ff2655b9a1287f5aa96b51cd1906c672805bf37
Smriti Tiwari <smrititiwari2016@gmail.com>
2020-02-07 22:36:46 +0530

    Update Prometheus_clientGO.go

--- Prometheus/Prometheus_clientGO.go
+++ Prometheus/Prometheus_clientGO.go
@@ -7,6 +7,15 @@ var jobsInQueue = prometheus.NewGauge(
     },
 }
 
+
+var jobsInQueue = prometheus.NewGaugeVec(
+    prometheus.GaugeOpts{
+        Name: "jobs_queued",
+        Help: "Current number of jobs queued",
+    },
+    []string{"job_type"},
+}
+    
 func init(){
     prometheus.GaugeOpts{
 }
@@ -14,12 +23,13 @@ func init(){
 func enqueueJob(job job) {
     queue.Add(job)
     jobsInQueue.Inc()
+    jobsQueued.WithLabelValues(job.type()).Inc()
 }
 
 func runNextJob() {
     job := queue.Dequeue()
     jobsInQueue.Dec()
-    
+    jobsQueued.WithLabelValues(job.type()).Dec()
     job.Run()
 }
 
