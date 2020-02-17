d4ccda5f1db74955c15e16ea6373351c12288eee
Smriti Tiwari <smrititiwari2016@gmail.com>
2020-02-17 19:24:52 +0530

    Update Gauge.go

--- Prometheus/Exposition/Gauge.go
+++ Prometheus/Exposition/Gauge.go
@@ -1,10 +1,14 @@
+// start by import of library 
 import "github.com/prometheus/client_golang/prometheus"
+
+// gauge for number of jobs currently in queue 
 
 var jobsQueued = prometheus.NewGauge(
     prometheus.GaugeOpts{
         Name: "jobs_queued",
         Help: "Current number of jobs queued",
     },
+    []string{"job_type"},
 )
 
 func init(){
